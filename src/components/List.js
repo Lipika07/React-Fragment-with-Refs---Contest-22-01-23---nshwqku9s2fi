a6158f7ad8be35e4b22ed33c78ec6a5994cb6e0c
Lipika Mohanty <110726592+Lipika07@users.noreply.github.com>
2023-01-22 11:32:24 +0530

    Update List.js

--- src/components/List.js
+++ src/components/List.js
@@ -3,11 +3,10 @@ import ListItems from './ListItems';
 
  const List = ({listx}) =>{
   return(
-    <div>
-    {listx.map((i) => {
-      return <ListItems valuex={i} />;
-    })}
-    </div>
+   <>
+   <ListItems valuex={listx}/>
+   </>
+   
   )
 }
 
